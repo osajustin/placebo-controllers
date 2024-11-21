1856fa5934aced19d4d764ceb134fd7ce40ab269
Justin Osagie <osajustin@gmail.com>
2024-11-21 00:49:06 -0500

    removed unused icons

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react'
 import { Dialog, DialogPanel } from '@headlessui/react'
-import { Bars3Icon, MagnifyingGlassCircleIcon, MagnifyingGlassIcon, ShoppingBagIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, ShoppingBagIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
     { name: 'Home', href: '/' },
